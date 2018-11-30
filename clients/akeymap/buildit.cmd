6b72426b561ce46a401bbf1fff44720b8d6cde6a
Joern Hinrichsen <joern.hinrichsen@ericsson.com>
2018-11-30 14:12:27 +0100

    cleanup path to interception

--- clients/akeymap/buildit.cmd
+++ clients/akeymap/buildit.cmd
@@ -4,5 +4,6 @@ SET PATH=%PATH%;..\..\tools
 SET WDK=C:\appWinDDK\7600.16385.1
 REM 32-bit:  %ComSpec% /c "scd . && pushd . && %WDK%\bin\setenv %WDK% fre x86 WXP no_oacr && popd && build"
 %ComSpec% /c "scd . && pushd . && %WDK%\bin\setenv %WDK% fre x64 WIN7 no_oacr && popd && build"
+
 copy ..\..\library\objfre_win7_amd64\amd64\interception.dll .\objfre_win7_amd64\amd64
 pause
