2f8f359e780f34a1b6d040be044ea319d35c8279
cajhin <cajhin@posteo.net>
2018-12-03 15:14:27 +0100

    wdk path

--- clients/capsicain/buildit.cmd
+++ clients/capsicain/buildit.cmd
@@ -1,7 +1,11 @@
 @echo off
 
 SET PATH=%PATH%;..\..\tools
-SET WDK=C:\appWinDDK\7600.16385.1
+if exist C:\appWinDDK\7600.16385.1 SET WDK=C:\appWinDDK\7600.16385.1
+else if exist C:\WinDDK\7600.16385.1 SET WDK=C:\WinDDK\7600.16385.1
+else echo WDK NOT FOUND
+
+
 REM 32-bit:  %ComSpec% /c "scd . && pushd . && %WDK%\bin\setenv %WDK% fre x86 WXP no_oacr && popd && build"
 %ComSpec% /c "scd . && pushd . && %WDK%\bin\setenv %WDK% fre x64 WIN7 no_oacr && popd && build"
 
