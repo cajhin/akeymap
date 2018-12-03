a74f82d7250339994b215f1ce48ee12751198f12
cajhin <cajhin@posteo.net>
2018-12-03 16:04:14 +0100

    dll again...

--- library/buildit.cmd
+++ library/buildit.cmd
@@ -4,4 +4,10 @@ echo buildit...
 
 SET PATH=%PATH%;..\tools
 
+if exist C:\appWinDDK\7600.16385.1 SET WDK=C:\appWinDDK\7600.16385.1
+if exist C:\WinDDK\7600.16385.1 SET WDK=C:\WinDDK\7600.16385.1
+echo WDK: %WDK%
+
 %ComSpec% /c "scd . && pushd . && %WDK%\bin\setenv %WDK% fre x64 WIN7 no_oacr && popd && build"
+
+pause
